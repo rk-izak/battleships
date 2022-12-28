8a6cbec4bd257d2d2bd154cc5375a54d184111e2
rk-izak <izakradoslaw@gmail.com>
2022-12-28 23:43:27 +0100

    added modular board

--- main.py
+++ main.py
@@ -1,9 +1,52 @@
-# 1->10 // A-->J
-letter_rows = {'A':0, 'B':1, 'C':2, 'D':3, 'E':4, 'F':5, 'G':6, 'H':7, 'I':8, 'J':9}
-number_rows = {'1 ':0, '2 ':1, '3 ':2, '4 ':3, '5 ':4, '6 ':5, '7 ':6, '8 ':7, '9 ':8, '10':9}
-print('    A', '  B', '  C', '  D', '  E', '  F', '  G', '  H', '  I', '  J')
-print('-------------------------------------------')
-for number in number_rows:
-        print((number + '|  {}'*len(letter_rows)).format(' ', ' ', ' ', ' ', ' ',
-                                              ' ', ' ', ' ', ' ', ' |'))
-        print('-------------------------------------------')
+import string
+alphabet = list(string.ascii_uppercase)
+
+
+class Board: # each player has one
+        # 1->10 // A-->J
+        def __init__(self, size=10): # size can be maximally 25, but 10 is preferred
+                self.size=size
+                self.alphabet = list(string.ascii_uppercase)[0:self.size]
+                self.numbers = list(range(len(self.alphabet)))[0:self.size]
+                self.letter_rows = {v:k for (k,v) in enumerate(self.alphabet)}
+                self.number_rows = {str(v+1):k for (k,v) in enumerate(self.numbers)}
+                
+        def print_board(self):
+        
+                value_block = "|   "
+                mark_line = '----' * (self.size+1)
+                alphabet_line = "   ".join(self.alphabet)
+                print("    " + alphabet_line)
+                print(mark_line)
+                for number in self.number_rows:
+                        if int(number) < 10:
+                                print(('0'+number + value_block * len(self.letter_rows)) + "|")
+                        else:
+                                print((number + value_block * len(self.letter_rows)) + "|")
+                        print(mark_line)
+
+        
+# ship classes start here:        
+class Carrier: # length: 5 count: 1
+        def __init__(self):
+                self.length = 5
+                self.hit = 0
+class Battleship: # length: 4 count: 1
+        def __init__(self):
+                self.length = 4
+                self.hit = 0
+class Cruiser: # length: 3 count: 1
+        def __init__(self):
+                self.length = 3
+                self.hit = 0
+class Destroyer: # length: 2 count: 2
+        def __init__(self):
+                self.length = 2
+                self.hit = 0
+class Submarine: # length: 1 count: 2
+        def __init__(self):
+                self.length = 1
+                self.hit = 0
+                
+player_1_board = Board()
+player_1_board.print_board()
